bb1415f2746aa9bf2e419a4f6cfac7856a941cd0
Artem <golgoffa1@gmail.com>
2025-10-14 19:54:48 +0300

    Add Util <3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://@localhost:3306/jdbc_hibernate";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "2sWHfBZvLF.";
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() {
+
+        try {
+            Class.forName(DRIVER);
+
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException("Не удалось загрузить класс драйвера: ", e);
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
 }
