3273cef20a1a4d0ef2344935e330c142229d1b44
Artem <golgoffa1@gmail.com>
2025-10-14 21:58:15 +0300

    tasks completed <3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Artem", "Iasko", (byte) 25);
+        userService.saveUser("Vova", "Petrosyan", (byte) 23);
+        userService.saveUser("Vage", "Gevorgyan", (byte) 27);
+        userService.saveUser("Erik", "Osipov", (byte) 29);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
